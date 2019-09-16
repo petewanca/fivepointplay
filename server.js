93418914f0e12522d7c2b688776bf9f561bdb357
TerrenceMM2 <terrencemm2@gmail.com>
2019-09-16 16:16:32 -0500

    adds passport dependencies

--- server.js
+++ server.js
@@ -3,7 +3,20 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+var passport = require("passport");
+var session = require("express-session");
+var bodyParser = require('body-parser');
+
 require("dotenv").config();
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//For passport
+app.use(session({ secret: 'password', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Setup Misc
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +26,9 @@ app.use(express.json());
 var db = require("./models");
 // Don't drop db by default
 var syncOptions = { force: false };
+
+//Load passport strategies
+require('./app/config/passport.js')(passport, db.user-model);
 
 // Define Routes
 require("./routes/api/index")(app);
