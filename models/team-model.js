78168ce610dba20f68d4e2b9e2e49fdda5d6f4be
TerrenceMM2 <terrencemm2@gmail.com>
2019-09-25 20:35:16 -0500

    pre-pull commit

--- models/team-model.js
+++ models/team-model.js
@@ -9,7 +9,7 @@ module.exports = function(sequelize, DataTypes) {
           type: DataTypes.STRING,
           allowNull: false
       },
-      img: {
+      teamLogo: {
           type: DataTypes.BLOB,
           allowNull: false,
           get() {
