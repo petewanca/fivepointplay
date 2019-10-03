ba5c2e0c725c512e92c2f93aa446ac28ae2be933
pete <petewanca@gmail.com>
2019-10-03 01:03:41 -0500

    player profile styling

--- client/src/pages/PlayerProfile.js
+++ client/src/pages/PlayerProfile.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+
+const styles = {
+  playerImage: {
+    width: "60%",
+    borderBottom: "1px solid gray",
+    marginBottom: 0,
+    paddingBottom: 0
+  },
+  teamLogo: {
+    width: "15%",
+    padding: 0,
+    margin: 0
+  },
+  profileText: {
+    padding: 0,
+    margin: 5,
+    fontSize: "1em"
+  },
+  statText: {
+    paddingLeft: "13%",
+    margin: 5,
+    fontSize: "1em",
+    textAlign: "left"
+  }
+}
 
 export default class PlayerProfile extends Component {
   state = {
     stats: [],
     fantasy: []
-  }
-
-  styles = {
-    // paper: {
-    //     padding: ".5rem"
-    // },
-    // image: {
-    //     maxWidth: "-webkit-fill-available"
-    // }
   }
   
   getStats = () => {
@@ -47,50 +64,35 @@ export default class PlayerProfile extends Component {
     const props = this.props.location.state.players;
 
     return (
-      <div id ={props.id} key={props.playerLink}>
-        <img src={props.playerImage} alt="player image"></img>
-        <img src={props.teamLogo} alt="team logo"></img>
-        <h1>{props.playerName}</h1>
-        <h2>{props.teamName}</h2>
-        <h3>{props.position}</h3>
-        <p>{props.height} / {props.weight} / {props.age} years old</p>
-
-        <h1>Last Season's Stats</h1>
-        <h5>*per game averages</h5>
-        <table>
-          <tr>
-            <th>Games</th>
-            <th>Minutes</th>
-            <th>Points</th>
-            <th>Rebounds</th>
-            <th>Assists</th>
-            <th>Steals</th>
-            <th>Blocks</th>
-            <th>Turnovers</th>
-            <th>Fouls</th>
-            <th>FG%</th>
-            <th>3P%</th>
-            <th>FT%</th>
-            <th>Fantasy</th>
-          </tr>
-          <tr>
-            <td>{this.state.stats.lsGamesPlayed}</td>
-            <td>{this.state.stats.lsMinutesPerGame}</td>
-            <td>{this.state.stats.lsPointsPerGame}</td>
-            <td>{this.state.stats.lsRebounds}</td>
-            <td>whoops!</td>
-            <td>{this.state.stats.lsSteals}</td>
-            <td>{this.state.stats.lsBlocks}</td>
-            <td>{this.state.stats.lsTurnovers}</td>
-            <td>{this.state.stats.lsFouls}</td>
-            <td>{this.state.stats.lsFieldGoalPercentage}</td>
-            <td>{this.state.stats.lsThreePointPercentage}</td>
-            <td>{this.state.stats.lsFreeThrowPercentage}</td>
-            <td>{this.state.fantasy.fantasyValue}</td>
-          </tr>
-        </table>
-        
-      </div>
+      <Grid justify="center" container spacing={3}>
+        <Grid item xs={12} md={6} lg={6} xl={6}>
+          <img style={styles.playerImage} src={props.playerImage} alt="profile pic"></img>
+        </Grid>
+        <Grid item xs={12} md={3} lg={3} xl={3}>
+          <img style={styles.teamLogo} src={props.teamLogo} alt="team logo"></img>
+          <h1 >{props.playerName}</h1>
+          <h2 style={styles.profileText}>{props.position}  |  {props.teamName}</h2>
+          <h3 style={styles.profileText}>{props.height} / {props.weight} / {props.age} years old</h3>
+          <h1 style={styles.profileText}>Fantasy Points Per Game: {this.state.fantasy.fantasyValue} fppg</h1>
+        </Grid>
+        <Grid item xs={12} md={3} lg={3} xl={3}>
+          <h2 style={styles.profileText}>Last Season's Stats</h2>
+          <h5 style={styles.profileText}>*per game averages</h5>
+          <br></br>
+          <h3 style={styles.statText}>Games played: {this.state.stats.lsGamesPlayed}</h3>
+          <h3 style={styles.statText}>Minutes: {this.state.stats.lsMinutesPerGame}</h3>
+          <h3 style={styles.statText}>Points: {this.state.stats.lsPointsPerGame}</h3>
+          <h3 style={styles.statText}>Rebounds: {this.state.stats.lsRebounds}</h3>
+          <h3 style={styles.statText}>Assists: whoops!</h3>
+          <h3 style={styles.statText}>Steals: {this.state.stats.lsSteals}</h3>
+          <h3 style={styles.statText}>Blocks: {this.state.stats.lsBlocks}</h3>
+          <h3 style={styles.statText}>Turnovers: {this.state.stats.lsTurnovers}</h3>
+          <h3 style={styles.statText}>Fouls: {this.state.stats.lsFouls}</h3>
+          <h3 style={styles.statText}>FG: {this.state.stats.lsFieldGoalPercentage}%</h3>
+          <h3 style={styles.statText}>3P: {this.state.stats.lsThreePointPercentage}%</h3>
+          <h3 style={styles.statText}>FT: {this.state.stats.lsFreeThrowPercentage}%</h3>
+        </Grid>
+      </Grid>
     )
   }
 }
