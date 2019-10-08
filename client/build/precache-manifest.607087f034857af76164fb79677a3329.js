cebc6c956dc0f09b45d8246061d9d03c4e7f33e0
pete <petewanca@gmail.com>
2019-10-07 19:54:16 -0500

    adds deployment build

---
+++ client/build/precache-manifest.607087f034857af76164fb79677a3329.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bc78bbeb73c6f650fbb9fefae3a096",
+    "url": "/petewanca/fivepointplay/index.html"
+  },
+  {
+    "revision": "63962d7d7877898e8586",
+    "url": "/petewanca/fivepointplay/static/css/main.8655dd47.chunk.css"
+  },
+  {
+    "revision": "bc2593713db4fbfd31dc",
+    "url": "/petewanca/fivepointplay/static/js/2.fe45b124.chunk.js"
+  },
+  {
+    "revision": "63962d7d7877898e8586",
+    "url": "/petewanca/fivepointplay/static/js/main.63f79abe.chunk.js"
+  },
+  {
+    "revision": "e27db00cf36097709829",
+    "url": "/petewanca/fivepointplay/static/js/runtime~main.8abcb476.js"
+  },
+  {
+    "revision": "16ce929f68669ac9ce082484eb468f1e",
+    "url": "/petewanca/fivepointplay/static/media/Antonio-Bold.16ce929f.ttf"
+  },
+  {
+    "revision": "bf125f57ae3b40fb449d78bfe0614cb4",
+    "url": "/petewanca/fivepointplay/static/media/Antonio-Bold.bf125f57.woff"
+  },
+  {
+    "revision": "c68fcc17ca15696c4dc8cda8faa19514",
+    "url": "/petewanca/fivepointplay/static/media/Antonio-Bold.c68fcc17.woff2"
+  },
+  {
+    "revision": "001336e1320c98531eccd50fe84807bc",
+    "url": "/petewanca/fivepointplay/static/media/Antonio-Light.001336e1.woff"
+  },
+  {
+    "revision": "6fd2e48e9195ff50e2785be2a6342cc9",
+    "url": "/petewanca/fivepointplay/static/media/Antonio-Light.6fd2e48e.woff2"
+  },
+  {
+    "revision": "a0ac16811044d61bb2c3a0a8d1dc0ee2",
+    "url": "/petewanca/fivepointplay/static/media/Antonio-Light.a0ac1681.ttf"
+  },
+  {
+    "revision": "28bff271e1c64baa56dbe49706f28fb4",
+    "url": "/petewanca/fivepointplay/static/media/Antonio-Regular.28bff271.woff"
+  },
+  {
+    "revision": "9f114fc98d6a9b3fd892c50d41c267c1",
+    "url": "/petewanca/fivepointplay/static/media/Antonio-Regular.9f114fc9.ttf"
+  },
+  {
+    "revision": "d8f708ec13ed451e94e8f7419333fbd9",
+    "url": "/petewanca/fivepointplay/static/media/Antonio-Regular.d8f708ec.woff2"
+  }
+]);
